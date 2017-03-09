42208d6297c3c19598d5790bd227546a13d2ba96
lasdou <florian15@gmail.com>
2017-03-09 11:54:26 +0100

    Update checkDeamons.php

--- checkDeamons.php
+++ checkDeamons.php
@@ -4,58 +4,63 @@ include('./deamon_functions.php');
 //get action passed by argument via CLI
 $action = isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : '';
 
-//list of deamons to supervise, path relative
-$deamons = array('deamon.php');
+//get php deamon filename (relative to this script) passed by argument via CLI
+$deamon = isset($_SERVER['argv'][2]) ? $_SERVER['argv'][2] : '';
+
+if($deamon == '')
+{
+    trigger_error('No deamon specified.', E_USER_ERROR);
+}
+
+if (!file_exists(dirname(__FILE__) .'/'. $deamon))
+{
+    trigger_error("Script $deamon does not exists.", E_USER_ERROR);
+}
 
 //determine action to execute
 $action = (preg_match("/^start$|^stop$/", $action)) ? $action : 'check';
 
-foreach($deamons as $deamon)
-{
-    switch ($action) {
-        case 'start' :
-            start_deamon($deamon);
-            exit ("Deamon $deamon launched.\n");
-            break;
+switch ($action) {
+    case 'start' :
+        start_deamon($deamon);
+        exit ("Deamon $deamon launched.\n");
+        break;
 
-		case 'stop' :
-		  stop_deamon($deamon);
-		  exit ("Deamon $deamon stopped.\n");
-		  break;
+    case 'stop' :
+          stop_deamon($deamon);
+          exit ("Deamon $deamon stopped.\n");
+          break;
 
-        case 'check' :
-            if (get_pid($deamon) > 0) {
-                echo ("$deamon is running.\n");
-                
-                $script = dirname(__FILE__) . '/'. $deamon;
+    case 'check' :
+        if (get_pid($deamon) > 0) {
+            echo ("$deamon is running.\n");
 
-                //get last edit time of deamon
-                $script_time = dirname(__FILE__).'/time_' . $deamon.'.txt';
+            $script = dirname(__FILE__) . '/'. $deamon;
 
-                //check if deamon was modified if true we relaunch him
-                if (!file_exists($script_time) or filemtime($script) > file_get_contents($script_time))
+            //get last edit time of deamon
+            $script_time = dirname(__FILE__).'/time_' . $deamon.'.txt';
+
+            //check if deamon was modified if true we relaunch him
+            if (!file_exists($script_time) or filemtime($script) > file_get_contents($script_time))
+            {
+                //save last edit time of deamon
+                file_put_contents($script_time, filemtime($script));
+
+                //restart deamon
+                if(stop_deamon($deamon) === true)
                 {
-                    //save last edit time of deamon
-                    file_put_contents($script_time, filemtime($script));
-
-                    //restart deamon
-                    if(stop_deamon($deamon) === true)
-                    {
-                        start_deamon($deamon);
-                        echo ("$deamon modified relaunched.\n");
-                    }
-                }
-                
-            } else {
                 start_deamon($deamon);
-                echo ("$deamon not running launched.\n");
+                echo ("$deamon modified relaunched.\n");
+                }
             }
-            break;
+        } else {
+            start_deamon($deamon);
+            echo ("$deamon not running launched.\n");
+        }
+        break;
 
-        default :
-            trigger_error('Wrong Parameters.', E_USER_ERROR);
-            break;
-    }
+    default :
+        trigger_error('Wrong Parameters.', E_USER_ERROR);
+        break;
 }
-
 exit("<success>\n");
